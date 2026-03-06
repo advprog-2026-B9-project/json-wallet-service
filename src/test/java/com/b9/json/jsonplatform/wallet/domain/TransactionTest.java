019f0ba5d961b181c5a9be854bd2b02a8902c17d
anjeelika <azzahraanjelikaborselano@gmail.com>
2026-03-06 20:34:27 +0700

    remove unused imports in transaction test

--- src/test/java/com/b9/json/jsonplatform/wallet/domain/TransactionTest.java
+++ src/test/java/com/b9/json/jsonplatform/wallet/domain/TransactionTest.java
@@ -1,13 +1,10 @@
 package com.b9.json.jsonplatform.wallet.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class TransactionTest {
     @Test
